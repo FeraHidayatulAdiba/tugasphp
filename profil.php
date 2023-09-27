14007f589a651c5e6897e923ea3f0b96299c7c65
FeraHidayatulAdiba <haiferahdytladiba@gmail.com>
2023-09-27 20:32:25 +0700

    ini tugas4

---
+++ profil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+    <div class="kontainer">
+        <div class="profil-box">
+            <img src="img/profil.jpg" class="profil-pic">
+            <h3>Fera Hidayatul Adiba</h3>
+            <p>Hallo saya dari univerisitas Nusantara PGRI kediri disini saya sedang mengikuti kegiatan Studi Mandiri yang diakan oleh kemendikbud, 
+                kebetulan saya masih berada di semester 5 dan aktif dalam pembelajaran online maupun offline. 
+                Disini saya akan mencoba menjelaskan ke pribadian saya jadi saya itu.</p>
+                <div class="links">
+                    <a href="https://t.me/@ferahidayatul"><img src="img/tele.png"></a>
+                    <a href="https://instagram.com/fera_hdytl?igshid=MzNlNGNkZWQ4Mg=="><img src="img/ig.png"></a>
+                    <a href="https://bit.ly/3qSxBly"><img src="img/wa.png"></a>
+                </div>
+
+        </div>
+    </div>
+</body>
+</html>
